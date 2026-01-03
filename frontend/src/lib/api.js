faaa949ea546741142eaee8c8423a2fc81982f1f
Ömercan Kaynar <138853606+omercankynr@users.noreply.github.com>
2026-01-03 18:06:10 +0300

    api base url fallback

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_BASE_URL;
+const baseURL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
 
 export const api = axios.create({
-  baseURL: baseURL,
+  baseURL,
   timeout: 20000,
 });
 
